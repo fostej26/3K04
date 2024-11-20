0b1a671d56b1d13a5a8cd646a9ea53bc989189d3
unloadinggnat <jaavinjm123@gmail.com>
2024-11-19 20:10:31 -0500

    started work on serial packet data, with params obj

--- serial_transmission.py
+++ serial_transmission.py
@@ -23,6 +23,36 @@ def output_data(array, count):
 
 #def send_params_to_pacemaker()
 
+class Params:
+    def __init__(self, pacing_mode, lower_rate_limit, upper_rate_limit, atrial_amplitude, atrial_pulse_width,
+                     atrial_sensitivity, ventricular_amplitude, ventricular_pulse_width, ventricular_sensitivity,
+                     vrp, arp, fixed_av_delay, maximum_sensor_rate, reaction_time, response_factor,
+                     recovery_time, activity_threshold):
+            self.pacing_mode = pacing_mode
+            self.lower_rate_limit = lower_rate_limit
+            self.upper_rate_limit = upper_rate_limit
+            self.atrial_amplitude = atrial_amplitude
+            self.atrial_pulse_width = atrial_pulse_width
+            self.atrial_sensitivity = atrial_sensitivity
+            self.ventricular_amplitude = ventricular_amplitude
+            self.ventricular_pulse_width = ventricular_pulse_width
+            self.ventricular_sensitivity = ventricular_sensitivity
+            self.vrp = vrp
+            self.arp = arp
+            self.fixed_av_delay = fixed_av_delay
+            self.maximum_sensor_rate = maximum_sensor_rate
+            self.reaction_time = reaction_time
+            self.response_factor = response_factor
+            self.recovery_time = recovery_time
+            self.activity_threshold = activity_threshold
+
+
+class SerialPacket:
+    # map serial packet data in here creating a params obj in the init
+
+    pass
+
+
 
 
 while flag:
