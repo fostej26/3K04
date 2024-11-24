c7cb840dce6332887c7798917b687b7d927d00ef
Tyler Smith <smitht71@mcmaster.ca>
2024-11-24 17:52:51 -0500

    Updates to proper recieve structure in remastered serial transmission

--- serial_transmission_remastered.py
+++ serial_transmission_remastered.py
@@ -6,7 +6,7 @@ import serial
 # Serial port setup
 port = serial.Serial()
 port.baudrate = 115200
-port.port = 'COM3'
+port.port = 'COM6'
 port.timeout = 10
 
 port.open()
@@ -41,23 +41,22 @@ def write_to_json(username, params_dict, file_name='parameters.json'):
 # Class to store parameters
 class Params:
     def __init__(self, data):
-        self.sync_byte = data[0]
-        self.mode = data[1]
-        self.lrl = data[2]
-        self.url = data[3]
-        self.atr_pulse_amplitude = struct.unpack('f', data[4:8])
-        self.atr_pulse_width = struct.unpack('f', data[8:12])
-        self.vent_pulse_amplitude = struct.unpack('f', data[12:16])
-        self.vent_pulse_width = struct.unpack('f', data[16:20])
-        self.vrp = struct.unpack('H', data[20:22])
-        self.arp = struct.unpack('H', data[22:24])
-        self.max_sensor_rate = data[24]
-        self.reaction_time = data[25]
-        self.response_factor = data[26]
-        self.recovery_time = data[27]
-        self.activity_threshold = data[28]
-        self.atrial_data = list(struct.unpack('10f', data[29:69]))
-        self.ventricle_data = list(struct.unpack('10f', data[78:108]))
+        self.mode = data[0]
+        self.lrl = data[1]
+        self.url = data[2]
+        self.atr_pulse_amplitude = struct.unpack('f', data[3:7])
+        self.atr_pulse_width = struct.unpack('f', data[7:11])
+        self.vent_pulse_amplitude = struct.unpack('f', data[11:15])
+        self.vent_pulse_width = struct.unpack('f', data[15:19])
+        self.vrp = struct.unpack('H', data[19:21])
+        self.arp = struct.unpack('H', data[21:23])
+        self.max_sensor_rate = data[23]
+        self.reaction_time = data[24]
+        self.response_factor = data[25]
+        self.recovery_time = data[26]
+        self.activity_threshold = data[27]
+        self.atrial_data = list(struct.unpack('10f', data[28:68]))
+        self.ventricle_data = list(struct.unpack('10f', data[68:108]))
 
     def to_dict(self):
 
@@ -88,20 +87,15 @@ if username not in users:
 # Main data processing loop
 while flag:
     first_byte = port.read(1)
-    if len(first_byte) == 1 and int.from_bytes(first_byte, byteorder='big') == 1:
-        second_byte = port.read(1)
-        if len(second_byte) == 1 and int.from_bytes(second_byte, byteorder='big') == 1:
-            arr = bytearray(137)
-            port.readinto(arr)
+    if len(first_byte) == 1 and int.from_bytes(first_byte, byteorder='big') == 7:
+        arr = bytearray(108)
+        port.readinto(arr)
 
-            # Parse the byte array into parameters
-            params = Params(arr)
+        # Parse the byte array into parameters
+        params = Params(arr)
 
-            # Save the parameters under the given username
-            write_to_json(username, params.to_dict())
-        else:
-            port.reset_input_buffer()
-            print('Unexpected message format: flushing input and resetting')
+        # Save the parameters under the given username
+        write_to_json(username, params.to_dict())
     else:
         port.reset_input_buffer()
         print('Unexpected message format: flushing input and resetting')
